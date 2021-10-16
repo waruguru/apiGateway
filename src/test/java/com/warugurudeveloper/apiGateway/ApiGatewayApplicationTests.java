a32f10ae50649a133894441b93174de52227b07f
waruguru <rachelmuriithi57@gmail.com>
2021-10-16 13:22:30 +0300

    Initial commit

---
+++ src/test/java/com/warugurudeveloper/apiGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.warugurudeveloper.apiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
